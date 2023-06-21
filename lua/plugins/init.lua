e0c4b833ed8af40c631718df7696dee166c1f322
minirei <minrei101@gmail.com>
2023-06-21 23:06:07 +0800

    removed buffer & path from cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -186,8 +186,8 @@ local default_plugins = {
         "saadparwaiz1/cmp_luasnip",
         "hrsh7th/cmp-nvim-lua",
         "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
+        -- "hrsh7th/cmp-buffer",
+        -- "hrsh7th/cmp-path",
       },
     },
     opts = function()
